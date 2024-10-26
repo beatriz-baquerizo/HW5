7ebeb9beca271b4fc4d200cc10bd284844aef34a
LUC COMP 272 <woconnell@luc.edu>
2024-10-26 14:02:37 -0500

    Add files via upload
    Initial upload of files to the repro

---
+++ ProblemSolutions.java
@@ -0,0 +1,107 @@
+/******************************************************************
+ *
+ *   BEATRIZ SANTOS / 002
+ *
+ *   This java file contains the problem solutions of isSubSet, findKthLargest,
+ *   and sort2Arrays methods. You should utilize the Java Collection Framework for
+ *   these methods.
+ *
+ ********************************************************************/
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+class ProblemSolutions {
+
+    /**
+     * Method: isSubset()
+     *
+     * Given two arrays of integers, A and B, return whether
+     * array B is a subset if array A. Example:
+     *      Input: [1,50,55,80,90], [55,90]
+     *      Output: true
+     *      Input: [1,50,55,80,90], [55,90, 99]
+     *      Output: false
+     *
+     * The solution time complexity must NOT be worse than O(n).
+     * For the solution, use a Hash Table.
+     *
+     * @param list1 - Input array A
+     * @param list2 - input array B
+     * @return      - returns boolean value B is a subset of A.
+     */
+
+    public boolean isSubset(int list1[], int list2[]) {
+        // initialize to Sets
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+
+        // turn lists into Sets
+        for (Integer num : list1){
+            setA.add(num);
+        }
+
+        for (Integer num : list2){
+            setB.add(num);
+        }
+
+        if(setA.containsAll(setB)){
+            return true;
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Method: findKthLargest
+     *
+     * Given an Array A and integer K, return the k-th maximum element in the array.
+     * Example:
+     *      Input: [1,7,3,10,34,5,8], 4
+     *      Output: 7
+     *
+     * @param array - Array of integers
+     * @param k     - the kth maximum element
+     * @return      - the value in the array which is the kth maximum value
+     */
+
+    public int findKthLargest(int[] array, int k) {
+        Arrays.sort(array); // O(n log n)
+        return array[array.length - k]; // O(1)
+    }
+
+
+    /**
+     * Method: sort2Arrays
+     *
+     * Given two arrays A and B with n and m integers respectively, return
+     * a single array of all the elements in A and B in sorted order. Example:
+     *      Input: [4,1,5], [3,2]
+     *      Output: 1 2 3 4 5
+     *
+     * @param array1    - Input array 1
+     * @param array2    - Input array 2
+     * @return          - Sorted array with all elements in A and B.
+     */
+
+    public int[] sort2Arrays(int[] array1, int[] array2) {
+        // create new array, the size should be the combined length of the two arrays
+        int[] combined = new int[array1.length + array2.length];
+
+        // populate combined with first array
+        for (int i = 0; i < array1.length; i++) {
+            combined[i] = array1[i];
+        }
+        // populate combined with second array starting with were you left of
+        for (int i = array1.length; i < combined.length; i++){
+            combined[i] = array2[i - array1.length];
+        }
+
+        // now that we have concatenated the arrays, sort the sorted array
+        Arrays.sort(combined);
+
+        return combined;
+    }
+
+}
